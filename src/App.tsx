288217e9824f2203621b7155ae61df0263fcb720
JTK <rsatang5@naver.com>
2021-12-10 14:32:54 +0900

    feat: Remove shadow feature due to performance issue

--- src/App.tsx
+++ src/App.tsx
@@ -8,15 +8,8 @@ import "./App.scss";
 
 const App: React.VFC = () => {
   const currentColorHex = useSelector((state: AppState) => state.currentCalculatedColorHex);
-  const currentColorSaturation = Color(currentColorHex).saturationl();
-  const currentColorLightness = Color(currentColorHex).lightness();
 
   document.body.style.backgroundColor = currentColorHex;
-  if (currentColorSaturation + currentColorLightness >= 120 || currentColorLightness >= 80) {
-    document.body.classList.add("shadow");
-  } else {
-    document.body.classList.remove("shadow");
-  }
 
   return <MainPage />;
 };
